6b7cbabf6365d06a36bce5fcee225f6f82bf7059
Ike Ekwueme <cryptoekwu@gmail.com>
2020-04-23 10:55:24 -0500

    Date is displaying now
    Still working to properly display duration and stats page.

--- models/workout.js
+++ models/workout.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: Date ,
+    day: {
+        type: Date,
+        default: Date.now
+    } ,
     exercises: Array
-})
+});
 
 
 const Workout = mongoose.model("Workout", workoutSchema);
